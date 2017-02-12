10832da81bd0af7f9e82c29d20740be0d7024245
lixianseng <896984548@qq.com>
2017-02-12 20:05:24 +0800

    2-12

--- 项目/首页/js/shouye_move.js
+++ 项目/首页/js/shouye_move.js
@@ -2,3 +2,7 @@ var Omain4=document.getElementById("main4");
 	Omain4.onclick=function(){
 		location.href="../xiaoxitongzhi/xiaoxitongzhi.html";
 	}
+var Oyc=document.getElementById("YouChen");
+	Oyc.onclick=function(){
+		location.href="../youchengxiezhu/youchengxiezhu.html";
+	}
